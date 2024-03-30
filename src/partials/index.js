83cf2ab1c6df669ea30df701878a3e71d10e7a41
Arakdy Fedorov <xexex98@yandex.ru>
2024-03-31 01:47:09 +0300

    profile page

--- src/partials/index.js
+++ src/partials/index.js
@@ -1,5 +1,6 @@
 export { Button } from "./button";
 export { Input } from "./input";
+export { Link } from "./link";
 export { FormContainer } from "./form-container";
 export { Modal } from "./modal";
 export { ProfileInfoField } from "./profile-info-field";
