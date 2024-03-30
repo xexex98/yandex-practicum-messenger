8f0ee39aff7e201fddc5b06ccc481a7df15441b7
Arakdy Fedorov <xexex98@yandex.ru>
2024-03-31 01:12:56 +0300

    prettier fix and some profile page files

--- src/partials/index.js
+++ src/partials/index.js
@@ -2,3 +2,4 @@ export { Button } from "./button";
 export { Input } from "./input";
 export { FormContainer } from "./form-container";
 export { Modal } from "./modal";
+export { ProfileInfoField } from "./profile-info-field";
