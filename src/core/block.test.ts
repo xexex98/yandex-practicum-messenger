8a70e568dcb9ae21d6737c6c614ff6b1c3dcaa44
Arakdy Fedorov <xexex98@yandex.ru>
2024-05-27 23:03:29 +0300

    Настройка тестов

---
+++ src/core/block.test.ts
@@ -0,0 +1,32 @@
+import { expect } from "chai";
+import Block, { BlockProps } from "src/core/block";
+
+describe("Block", () => {
+  let PageClass: typeof Block;
+
+  before(() => {
+    class Page extends Block {
+      constructor(props: BlockProps) {
+        super({
+          ...props,
+        });
+      }
+
+      render(): string {
+        return `<div>
+                <span id="test-text">{{text}}</span>
+                <button>{{text-button}}</button>
+            </div>`;
+      }
+    }
+    PageClass = Page;
+  });
+  it("Должен создать компонент с состоянием из конструктора", () => {
+    const text = "Hello";
+    const pageComponent = new PageClass({ text });
+
+    // const spanText = pageComponent.element?.querySelector("#test-text")?.innerHTML;
+
+    // expect(spanText).to.be.eq(text);
+  });
+});
---
+++ src/test/mochaSetup.js
@@ -0,0 +1,8 @@
+import { JSDOM } from "jsdom";
+
+const jsdom = new JSDOM("<body></body>");
+
+global.window = jsdom.window;
+global.document = jsdom.window.document;
+global.Node = jsdom.window.Node;
+global.MouseEvent = jsdom.window.MouseEvent;
