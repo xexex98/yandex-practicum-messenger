6016f6152fb04250d99fb6250360d6f507d6d9f9
Arakdy Fedorov <xexex98@yandex.ru>
2024-05-28 20:52:36 +0300

    Husky and lint-staged

--- src/init/register-partials.ts
+++ src/init/register-partials.ts
@@ -3,8 +3,6 @@ import Handlebars from "handlebars";
 import * as Partials from "../partials";
 
 export const registerPartial = () => {
-  let a;
-
   Object.entries(Partials).forEach(([name, component]) => {
     if (typeof component === "string") {
       Handlebars.registerPartial(name, component);
