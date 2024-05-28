3f007bb61eb80b6a41cbdc26ccdfcfbd515ff7a3
Arakdy Fedorov <xexex98@yandex.ru>
2024-05-28 21:25:06 +0300

    Начало тестирования компонента блок

--- src/core/block.test.ts
+++ src/core/block.test.ts
@@ -1,34 +1,81 @@
 import { expect } from "chai";
-import Block, { BlockProps } from "src/core/block";
+import Handlebars from "handlebars";
+import sinon from "sinon";
+import Block from "src/core/block";
 
 describe("Block", () => {
-  let PageClass: typeof Block;
+  let block: Block;
+  let stubTemplate: sinon.SinonStub;
 
-  before(() => {
-    class Page extends Block {
-      constructor(props: BlockProps) {
-        super({
-          ...props,
-        });
-      }
+  beforeEach(() => {
+    stubTemplate = sinon.stub(Handlebars, "compile").returns(() => "<div></div>");
 
-      render(): string {
-        return `<div>
-                  <span id="test-text">{{text}}</span>
-                  <button>{{text-button}}</button>
-              </div>`;
-      }
-    }
-
-    PageClass = Page;
+    block = new Block();
   });
 
-  it("Должен создать компонент с состоянием из конструктора", () => {
-    const text = "Hello";
-    const pageComponent = new PageClass({ text });
+  afterEach(() => {
+    stubTemplate.restore();
+  });
 
-    const spanText = pageComponent.element?.querySelector("#test-text")?.innerHTML;
+  it("should initialize with an ID", () => {
+    expect(block).to.have.property("_id").that.is.a("string");
+  });
 
-    expect(spanText).to.be.eq(text);
+  it("should call componentDidMount on _componentDidMount", () => {
+    const spy = sinon.spy(block, "componentDidMount");
+
+    block["_componentDidMount"]();
+    expect(spy.calledOnce).to.be.true;
+  });
+
+  it("should render correctly", () => {
+    block["render"] = () => "<div>{{content}}</div>";
+    block["props"] = { content: "Hello, world!" };
+    block["_render"]();
+
+    const element = block.getContent();
+
+    console.log(element);
+    expect(element?.outerHTML).to.equal("<div>Hello, world!</div>");
+  });
+
+  it("should update props correctly", () => {
+    const oldProps = { content: "old" };
+    const newProps = { content: "new" };
+
+    block["props"] = oldProps;
+    block.setProps(newProps);
+
+    expect(block["props"]).to.deep.equal(newProps);
+  });
+
+  it("should add and remove events", () => {
+    const clickHandler = sinon.spy();
+
+    block["props"] = { events: { click: clickHandler } };
+
+    const element = document.createElement("div");
+
+    block["_element"] = element;
+    block["_addEvents"]();
+
+    element.click();
+    expect(clickHandler.calledOnce).to.be.true;
+
+    block["_removeEvents"]();
+    element.click();
+    expect(clickHandler.calledOnce).to.be.true;
+  });
+
+  it("should hide and show the element", () => {
+    const element = document.createElement("div");
+
+    block["_element"] = element;
+
+    block.hide();
+    expect(element.style.display).to.equal("none");
+
+    block.show();
+    expect(element.style.display).to.equal("block");
   });
 });
