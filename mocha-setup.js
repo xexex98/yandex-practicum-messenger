3878701c4da560da0fa8157683b33782f7424e8b
Arakdy Fedorov <xexex98@yandex.ru>
2024-05-30 22:33:42 +0300

    Правки кода mochasetup и xmlhttprequest

--- mocha-setup.js
+++ mocha-setup.js
@@ -1,4 +1,5 @@
 import { JSDOM } from "jsdom";
+import sinon from "sinon";
 
 const jsdom = new JSDOM(`<body></body>`);
 
@@ -6,3 +7,4 @@ global.window = jsdom.window;
 global.document = jsdom.window.document;
 global.Node = jsdom.window.Node;
 global.MouseEvent = jsdom.window.MouseEvent;
+global.XMLHttpRequest = sinon.useFakeXMLHttpRequest();
